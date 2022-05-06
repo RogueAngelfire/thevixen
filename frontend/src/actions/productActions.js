3cc371fbe505f1eda8f90d55e2579e4f9e473903
Robin Collins <info@robincollins.co.uk>
2022-05-06 15:23:36 +0100

    Added front end product reviews part 1

--- frontend/src/actions/productActions.js
+++ frontend/src/actions/productActions.js
@@ -19,6 +19,11 @@ import {
   PRODUCT_UPDATE_REQUEST,
   PRODUCT_UPDATE_SUCCESS,
   PRODUCT_UPDATE_FAIL,
+
+  PRODUCT_CREATE_REVIEW_REQUEST,
+  PRODUCT_CREATE_REVIEW_SUCCESS,
+  PRODUCT_CREATE_REVIEW_FAIL,
+
 } from '../constants/productConstants'
 
 export const listProducts = () => async (dispatch) => {
@@ -171,13 +176,51 @@ export const updateProduct = (product) => async (dispatch, getState) => {
 
       dispatch({
         type:PRODUCT_DETAILS_SUCCESS,
-        payload: data
+        payload: data,
       })
 
 
   }catch (error) {
     dispatch({
       type: PRODUCT_UPDATE_FAIL,
+      payload: error.response && error.response.data.detail
+        ? error.response.data.detail
+        : error.message
+    })
+  }
+}
+
+export const createProductReview = (productId, review) => async (dispatch, getState) => {
+  try{
+    dispatch({ 
+      type: PRODUCT_CREATE_REVIEW_REQUEST
+    })
+
+    const {
+      userLogin: { userInfo },
+    } = getState()
+
+    const config = {
+      headers:{
+        'content-type': 'application/json',
+        Authorization: `Bearer ${userInfo.token}`
+      }
+    }
+
+    const { data } = await axios.post(
+      `/api/products/update/${productId}/reviews/`,
+      review,
+      config
+    )
+    dispatch({
+      type:PRODUCT_CREATE_REVIEW_SUCCESS,
+      payload: data,
+    })
+
+
+  }catch (error) {
+    dispatch({
+      type: PRODUCT_CREATE_REVIEW_FAIL,
       payload: error.response && error.response.data.detail
         ? error.response.data.detail
         : error.message
