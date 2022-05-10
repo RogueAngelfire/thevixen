3e0635fae0d7a4873f6cadd289d8da70202cf2b5
Robin Collins <info@robincollins.co.uk>
2022-05-10 21:38:13 +0100

    Added frontend product review

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -31,13 +31,28 @@ function ProductScreen() {
   } = productReviewCreate
 
   useEffect(() => {
+    if(successProductReview){
+      setRating(0)
+      setComment('')
+      dispatch({ type:PRODUCT_CREATE_REVIEW_RESET })
+    }
+
     dispatch(listProductDetails(id))
     console.log(qty)
-  }, [dispatch, id, qty])
+  }, [dispatch, id, qty, successProductReview])
 
   const addToBasketHandler = () =>{
     console.log('added to basket', id, 'id number', qty, 'quantity added')
     navigate(`/basket/${id}?qty=${qty}`)
+  }
+  /// Issue here with id as not working and old params linked to previous react router dom
+  const submitHandler = (e) => {
+    e.preventDefault()
+    dispatch(createProductReview(
+      id, {
+        rating,
+        comment
+      }))
   }
 
   return (
@@ -48,6 +63,7 @@ function ProductScreen() {
         : error
         ? <Message variant='danger'>{error}</Message>
         :(
+          <div>
           <Row>
         <Col md={6}>
           {product && <Image src={product.image} alt={product.name} fluid />}
@@ -134,6 +150,72 @@ function ProductScreen() {
           </Card>
         </Col>
       </Row>
+
+      <Row>
+        <Col md={6}>
+          <h4>Reviews</h4>
+          {product.reviews.length === 0 && <Message variant='info'>No Submitted Reviews</Message>}
+          <ListGroup variant='flush'>
+            {product.reviews.map((review) => (
+              <ListGroup.Item key={review._id}>
+                <strong>{review.name}</strong>
+                <Rating value={review.rating} color='#f88e825'/>
+                <p>{review.createdAt.substring(0, 10)}</p>
+                <p>{review.comment}</p>
+              </ListGroup.Item>
+            ))}
+
+            <ListGroup.Item>
+              <h4>Write a Review</h4>
+
+             {loadingProductReview && <Loader/>} 
+             {successProductReview && <Message variant='success'>Review Submitted</Message>}
+             {errorProductReview && <Message variant='danger'>{errorProductReview}</Message>}
+
+              {userInfo ? (
+                <Form onSubmit={submitHandler}>
+                  <Form.Group controlId='rating'>
+                    <Form.Label>Rating</Form.Label>
+                    <Form.Control
+                      as='select'
+                      value={rating}
+                      onChange={(e) => setRating(e.target.value)}
+                    >
+                      <option value=''>Select...</option>
+                      <option value='1'>1 Poor</option>
+                      <option value='2'>2 Fair</option>
+                      <option value='3'>3 Good</option>
+                      <option value='4'>4 Exellent</option>
+                      <option value='5'>5 Classic</option>
+                    </Form.Control>
+                  </Form.Group>
+
+                  <Form.Group controlId='comment'>
+                    <Form.Label>Review</Form.Label>
+                    <Form.Control
+                      as='textarea'
+                      row='5'
+                      value={comment}
+                      onChange={(e) => setComment(e.target.value)}
+                    ></Form.Control>
+                  </Form.Group>
+
+                  <Button
+                    disabled={loadingProductReview}
+                    type='submit'
+                    variant='warning'
+                  >
+                    Submit
+                  </Button>
+                </Form>
+              ) : (
+                <Message variant='info'>Please <Link to='/login'>Login</Link> to write a review</Message>
+              )}
+            </ListGroup.Item>
+          </ListGroup>
+        </Col>
+      </Row>
+      </div>
         )
 
       }
